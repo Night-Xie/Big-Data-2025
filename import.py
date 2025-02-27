985dc9ad498d18612f2cae850de3aaf92d31689b
NightXie <chayanne.mascarenhas@ucdconnect.ie>
2025-02-27 14:37:49 +0000

    added import script to load csv -> sql

--- import.py
+++ import.py
@@ -1,12 +1,68 @@
 import pandas as pd
 import mysql.connector
+import sys
+import getpass
 
-conn = mysql.connector.connect(host="localhost", user="root", password="yourpass", database="BIG_DATA")
-cursor = conn.cursor()
+#check arg len 
+if len(sys.argv) != 2: 
+    print("Usage: python import.py <mysql_user>")
+    sys.exit(1)
 
-df = pd.read_csv("yourfile.csv")
-for _, row in df.iterrows():
-    cursor.execute("INSERT INTO your_table (col1, col2) VALUES (%s, %s)", (row["col1"], row["col2"]))
+#get command line args
+mysql_user = sys.argv[1]
+mysql_password = getpass.getpass(f"Enter password for user {mysql_user} (leave empty for no password):") or None
 
-conn.commit()
-conn.close()
+#connect to sql
+link_args = {
+    "host": "localhost",
+    "user": mysql_user,
+    "database": "BIG_DATA"
+}
+if mysql_password:
+    link_args["password"] = mysql_password
+
+# map csv : table_name
+
+source_sql_map = {
+    "data/RBI DATA states_wise_population_Income.csv": "RBI_DATA",
+    "data/internet-facility.csv": "SCHOOL_INTERNET_FACILITY",
+    "data/computer-availability.csv": "SCHOOL_COMPUTER_FACILITY",
+    "data/electricity-availability.csv": "SCHOOL_ELECTRICITY_FACILITY"
+}
+
+def get_columns(cursor, table_name):
+    cursor.execute(f"SHOW COLUMNS FROM {table_name}")
+    return [row[0] for row in cursor.fetchall()]
+
+# function to load data from csv to sql table
+def load(cursor, source_sql_map):
+    for csv_file, table_name in source_sql_map.items():
+        try:
+            df = pd.read_csv(csv_file)
+            columns = get_columns(cursor, table_name)
+
+            placeholders = ', '.join(['%s'] * len(df.columns))
+            columns_str = ', '.join([f"{col}" for col in columns])
+
+            sql = f"INSERT INTO {table_name} ({columns_str}) VALUES ({placeholders})"
+
+            rows = 0
+            for _, row in df.iterrows():
+                rows = rows+1
+                cursor.execute(sql, tuple(row))
+
+            print(f"Imported {rows} rows from {csv_file} into {table_name}.")
+
+        except Exception as err:
+            print(f"Error processing {csv_file}: {err}")
+
+# establish connection and load data
+link = mysql.connector.connect(**link_args)
+cursor = link.cursor()
+
+load(cursor, source_sql_map)
+
+# commit & close connection
+link.commit()
+cursor.close()
+link.close()
