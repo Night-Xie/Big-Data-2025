5ae86d786a32b74ba450d78c9f09302781db77d7
NightXie <chayanne.mascarenhas@ucdconnect.ie>
2025-03-14 20:32:49 +0000

    added sql queries for getting statewise data + national average neet mark

---
+++ sql/calculations.sql
@@ -0,0 +1,71 @@
+# -- -- National results -- --
+
+# Get average NEET mark (country-wide)
+SELECT AVG(marks) AS NATIONAL_AVERAGE_NEET_MARK
+FROM RESULTS;
+
+# -- -- State results -- -- --
+
+	# -- -- -- Neet Marks -- -- -- 
+
+#(state, STATE_AVERAGE_NEET_MARK)
+SELECT state, ROUND(AVG(marks),2) AS STATE_AVERAGE_NEET_MARK
+FROM RESULTS
+GROUP BY state;
+
+#(state, STATE_VS_NATIONAL_RATIO)
+SELECT state, ROUND(AVG(marks) / (SELECT AVG(marks) FROM RESULTS),2) AS STATE_VS_NATIONAL_RATIO
+FROM RESULTS
+GROUP BY state;
+
+	# -- -- -- Economic Status -- -- -- 
+
+#(state, GDP_2011, GDP_2021)
+SELECT state, `2011`, `2021`
+FROM INDIA
+WHERE category = "Gross State Domestic Product";
+
+#(state, AVG_ESTIMATED_UNEMPLOYMENT_RATE_2020)
+SELECT state, ROUND(AVG(estimated_unemployment_rate),2) AS AVG_ESTIMATED_UNEMPLOYMENT_RATE_2020
+FROM UNEMPLOYMENT_RATE
+GROUP BY state;
+
+#(state, POPULATION_2011, POPULATION_2021)
+SELECT state, `2011` AS POPULATION_2011, `2021` AS POPULATION_2021
+FROM POPULATION_DATA
+WHERE data = "Total Population";
+
+#(state, PER_CAPITA_INCOME_2011, PER_CAPITA_INCOME_2021)
+SELECT state, `2011` AS PER_CAPITA_INCOME_2011, `2021`AS PER_CAPITA_INCOME_2021
+FROM INDIA
+WHERE category = "Per Capita Income";
+
+#(state, POVERTY_RATE_2011)
+SELECT States_Union_Territories AS state, POVERTY_2011 AS POVERTY_RATE_2011
+FROM RBI_DATA;
+
+#(state, LITERACY_2011)
+SELECT state, `2011` AS LITERACY_2011
+FROM POPULATION_DATA
+WHERE data = "Literacy Rate";
+
+	# -- -- -- Education Infrastructure -- -- -- 
+
+#(state, SCHOOLS_WITH_INTERNET)
+SELECT State AS state, Percent_Schools_Internet_All_Management AS SCHOOLS_WITH_INTERNET
+FROM SCHOOL_INTERNET_FACILITY;
+
+#(state, SCHOOLS_WITH_COMPUTERS)
+SELECT State AS state, Percent_Schools_Computer_All_Management AS SCHOOLS_WITH_COMPUTERS
+FROM SCHOOL_COMPUTER_FACILITY;
+
+#(state, SCHOOLS_WITH_FUNCTIONAL_ELECTRICITY)
+SELECT State AS state, Percent_Schools_Functional_Electricity_All_Management AS SCHOOLS_WITH_FUNCTIONAL_ELECTRICITY
+FROM SCHOOL_ELECTRICITY_FACILITY;
+
+#(state, SCHOOLS_WITH_LIBRARIES)
+SELECT State AS state, Percent_Schools_with_Library_All_Management AS SCHOOLS_WITH_LIBRARIES
+FROM LIBRARY_FACILITY;
+
+
+
