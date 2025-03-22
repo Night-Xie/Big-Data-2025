cc65e10b4db88a3348f633d68eabd241edb0a71e
NightXie <chayanne.mascarenhas@ucdconnect.ie>
2025-03-22 12:18:47 +0000

    updated all queries with execution time

--- sql/calculations.sql
+++ sql/calculations.sql
@@ -1,6 +1,6 @@
 # -- -- National results -- --
 
-# Get average NEET mark (country-wide)
+# Get average NEET mark (country-wide) // 0.98 sec
 SELECT AVG(marks) AS NATIONAL_AVERAGE_NEET_MARK
 FROM RESULTS;
 
@@ -8,24 +8,24 @@ FROM RESULTS;
 
 	# -- -- -- Neet Marks -- -- -- 
 
-#(state, STATE_AVERAGE_NEET_MARK)
+#(state, STATE_AVERAGE_NEET_MARK) // 2.44 sec
 SELECT state, ROUND(AVG(marks),2) AS STATE_AVERAGE_NEET_MARK
 FROM RESULTS
 GROUP BY state;
 
-#(state, STATE_VS_NATIONAL_RATIO)
+#(state, STATE_VS_NATIONAL_RATIO) // 2.79 sec 
 SELECT state, ROUND(AVG(marks) / (SELECT AVG(marks) FROM RESULTS),2) AS STATE_VS_NATIONAL_RATIO
 FROM RESULTS
 GROUP BY state;
 
 	# -- -- -- Economic Status -- -- -- 
 
-#(state, GDP_2011, GDP_2021)
+#(state, GDP_2011, GDP_2021) // 0.01 sec
 SELECT state, `2011` AS GDP_2011, `2021` AS GDP_2021
 FROM INDIA
 WHERE category = "Gross State Domestic Product";
 
-#GDP per Capita 
+#(state, GDP_PER_CAPITA_2011, GDP_PER_CAPITA_2021) // 0.00 sec
 SELECT INDIA.state, 
 	   ROUND(INDIA.`2011` /  POPULATION_DATA.`2011`,2) AS GDP_PER_CAPITA_2011,
 	   ROUND(INDIA.`2021` / POPULATION_DATA.`2011`,2) AS GDP_PER_CAPITA_2021
@@ -33,45 +33,45 @@ FROM INDIA JOIN POPULATION_DATA
 ON INDIA.state = POPULATION_DATA.state
 WHERE INDIA.category = "Gross State Domestic Product" AND POPULATION_DATA.data = "Total Population";
 
-#(state, AVG_ESTIMATED_UNEMPLOYMENT_RATE_2020)
+#(state, AVG_ESTIMATED_UNEMPLOYMENT_RATE_2020) // 0.01 sec
 SELECT state, ROUND(AVG(estimated_unemployment_rate),2) AS AVG_ESTIMATED_UNEMPLOYMENT_RATE_2020
 FROM UNEMPLOYMENT_RATE
 GROUP BY state;
 
-#(state, POPULATION_2011, POPULATION_2021)
+#(state, POPULATION_2011, POPULATION_2021) // 0.00 sec
 SELECT state, `2011` AS POPULATION_2011, `2021` AS POPULATION_2021
 FROM POPULATION_DATA
 WHERE data = "Total Population";
 
-#(state, PER_CAPITA_INCOME_2011, PER_CAPITA_INCOME_2021)
+#(state, PER_CAPITA_INCOME_2011, PER_CAPITA_INCOME_2021) // 0.01 sec
 SELECT state, `2011` AS PER_CAPITA_INCOME_2011, `2021`AS PER_CAPITA_INCOME_2021
 FROM INDIA
 WHERE category = "Per Capita Income";
 
-#(state, POVERTY_RATE_2011)
+#(state, POVERTY_RATE_2011) // 0.00 sec
 SELECT States_Union_Territories AS state, POVERTY_2011 AS POVERTY_RATE_2011
 FROM RBI_DATA;
 
-#(state, LITERACY_2011)
+#(state, LITERACY_2011) // 0.00 sec
 SELECT state, `2011` AS LITERACY_2011
 FROM POPULATION_DATA
 WHERE data = "Literacy Rate";
 
 	# -- -- -- Education Infrastructure -- -- -- 
 
-#(state, SCHOOLS_WITH_INTERNET)
+#(state, SCHOOLS_WITH_INTERNET) // 0.00 sec
 SELECT State AS state, Percent_Schools_Internet_All_Management AS SCHOOLS_WITH_INTERNET
 FROM SCHOOL_INTERNET_FACILITY;
 
-#(state, SCHOOLS_WITH_COMPUTERS)
+#(state, SCHOOLS_WITH_COMPUTERS) // 0.01 sec
 SELECT State AS state, Percent_Schools_Computer_All_Management AS SCHOOLS_WITH_COMPUTERS
 FROM SCHOOL_COMPUTER_FACILITY;
 
-#(state, SCHOOLS_WITH_FUNCTIONAL_ELECTRICITY)
+#(state, SCHOOLS_WITH_FUNCTIONAL_ELECTRICITY) // 0.00 sec
 SELECT State AS state, Percent_Schools_Functional_Electricity_All_Management AS SCHOOLS_WITH_FUNCTIONAL_ELECTRICITY
 FROM SCHOOL_ELECTRICITY_FACILITY;
 
-#(state, SCHOOLS_WITH_LIBRARIES)
+#(state, SCHOOLS_WITH_LIBRARIES) // 0.00 sec
 SELECT State AS state, Percent_Schools_with_Library_All_Management AS SCHOOLS_WITH_LIBRARIES
 FROM LIBRARY_FACILITY;
 
