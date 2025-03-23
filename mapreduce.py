5f58b6eb4fa32db8c7373f75488c993f6f2f01f6
NightXie <chayanne.mascarenhas@ucdconnect.ie>
2025-03-23 17:25:34 +0000

    updated to include results.collect() in timing

--- mapreduce.py
+++ mapreduce.py
@@ -29,10 +29,8 @@ parsed = results.map(parser).filter(lambda row: row is not None)
 # map results to kv pair (state, (marks, 1))
 mapped = parsed.map(lambda row: (row["state"], (row["marks"], 1)))
 
-
 # reduce to sum(marks), count 
 reduced = mapped.reduceByKey(lambda p1,p2 : (p1[0] + p2[0], p1[1] + p2[1]))
-
 
 # get averages 
 total = reduced.map(lambda x: x[1]).reduce(lambda p1,p2: (p1[0] + p2[0], p1[1] + p2[1]))
@@ -42,13 +40,14 @@ state_average = reduced.mapValues(lambda p: p[0] / p[1])
 state_vs_national_ratio = state_average.mapValues(lambda s_avg: s_avg/national_average)
 
 results = state_average.join(state_vs_national_ratio)
+data = results.collect()
 
 # record finish time
 end = time.time()
 
 # output results
 print(f"National Average : {national_average}")
-formatted_results = [(state, avg, ratio) for state, (avg, ratio) in results.collect()]
+formatted_results = [(state, avg, ratio) for state, (avg, ratio) in data]
 col_names = ["state", "average_mark", "ratio_vs_national"]
 print(tabulate(formatted_results, headers=col_names, tablefmt="fancy_grid"))
 
